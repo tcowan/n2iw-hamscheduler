4d55123c3e767ef836470000095b567de5b5a8e2
lacym350 <109993756+lacym350@users.noreply.github.com>
2023-01-18 17:09:39 -0500

    added views to admin function

---
+++ choose_user_form.php
@@ -0,0 +1,10 @@
+<form action="<?= $url ?>" method="GET">
+    <fieldset>
+        <div class="control-group">
+            <input autofocus name="call" placeholder="Call sign" type="text"/>
+        </div>
+        <div class="control-group">
+            <button type="submit" class="btn"><?= $action ?></button>
+        </div>
+    </fieldset>
+</form>
